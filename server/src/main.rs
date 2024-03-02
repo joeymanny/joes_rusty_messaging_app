17062213f651b9c179fd091f06527134748b5ae4
joeymanny <joeymanny1212@gmail.com>
2024-03-01 20:33:52 -0900

    nicer

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,7 @@ use std::{io::Read, net::Ipv4Addr};
 
 fn main() {
     let listener = std::net::TcpListener::bind(std::net::SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), lib::SOCKET)).expect("problem binding 127.0.0.1");
+    println!("serving on 127.0.0.1");
     loop{
         let (stream, _) = listener.accept().expect("problem accepting tcp stream");
         let data = stream.bytes().map(|v| char::from_u32(v.unwrap() as u32).unwrap()).collect::<String>();
