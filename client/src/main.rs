b3ce563362b643a0d67e6078ed9c04faa0ac1547
joeymanny <joeymanny1212@gmail.com>
2024-03-04 20:56:58 -0900

    now uses eframe for gui

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,149 +1,179 @@
-use clap::Parser;
-use lib::{Message, LoginResult};
-use owo_colors::OwoColorize;
-use std::{io::Write, net::SocketAddr};
+use std::str::FromStr;
 
-#[derive(clap::Parser, Debug)]
-struct Arguments {
-    #[arg(short = 's', long = "server")]
-    server: Option<std::net::IpAddr>,
-}
+use lib::Message;
 
 fn main() {
-    let mut main_menu_choice = String::new();
-    let mut server_addr = if let Arguments { server: Some(ip) } = Arguments::parse() {
-        Some(std::net::SocketAddr::new(ip, lib::PORT))
-    } else {
-        eprintln!(
-            "{} no server address configured, use the `server` menu to set one",
-            "notice".yellow()
-        );
-        None
+    let options = eframe::NativeOptions::default();
+    eframe::run_native("My App", options, Box::new(|cc| Box::new(MyApp::new(cc)))).unwrap();
+}
+struct MyApp {
+    menu: Menu,
+    server: Option<std::net::IpAddr>,
+    ip_submission: String,
+}
+#[derive(Clone)]
+enum Menu {
+    Login {
+        username: String,
+        password: String,
+        login_failure: Option<LoginResult>,
+    },
+    Contacts,
+    Chat {
+        user_id: u32,
+    },
+}
+#[derive(Clone, Copy, Debug)]
+enum LoginResult {
+    NoServer,
+    ConnectionTimeout,
+    NetworkError,
+    SomethingWentWrong,
+    BadUser,
+    BadPass,
+    Success,
+}
+
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        cc.egui_ctx.set_style(egui::style::Style {
+            override_text_style: Some(egui::style::TextStyle::Monospace),
+            ..egui::style::Style::default()
+        });
+        MyApp {
+            menu: Menu::Login {
+                username: String::new(),
+                password: String::new(),
+                login_failure: None,
+            },
+            server: None,
+            ip_submission: String::default(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::bottom("server panel").show(ctx, |ui| {
+            ui.label(format!(
+                "current server: {}",
+                if let Some(v) = self.server {
+                    v.to_string()
+                } else {
+                    "None".to_owned()
+                }
+            ));
+            if ui
+                .text_edit_singleline(&mut self.ip_submission)
+                .lost_focus()
+            {
+                self.server = self.ip_submission.parse().ok();
+                self.ip_submission.clear();
+            };
+        });
+
+        match self.menu {
+            Menu::Login { .. } => login_menu(self, ctx, frame),
+            Menu::Contacts => contacts_menu(self, ctx, frame),
+            Menu::Chat { user_id } => chat_menu(self, ctx, frame, user_id),
+        }
+    }
+}
+fn login_menu(app_state: &mut MyApp, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    let (username, password, login_failure) = match app_state.menu {
+        Menu::Login {
+            ref mut username,
+            ref mut password,
+            ref mut login_failure,
+        } => (username, password, login_failure),
+        _ => panic!("this is unreachable"),
     };
-    'main: loop {
-        println!("welcome. menu options are:\n    login\tlog in to messaging app\n    server\tdefine a server (menu)\n    exit  \texit the program");
-        std::io::stdin()
-            .read_line(&mut main_menu_choice)
-            .expect(lib::ERR_MSG_STDIN);
-        match main_menu_choice.trim() {
-            "login" => handle_login(server_addr),
-            "exit" => break 'main,
-            "server" => server_address_menu(&mut server_addr),
-            _ => println!("{} unrecognized menu option", "error".red()),
+    let mut login_now = false;
+    egui::CentralPanel::default().show(ctx, |ui| {
+        ui.vertical_centered(|ui| {
+            ui.label("Welcome to login");
+            egui::TextEdit::singleline(username)
+                .hint_text("username")
+                .show(ui);
+            egui::TextEdit::singleline(password)
+                .password(true)
+                .hint_text("password")
+                .show(ui);
+            login_now = ui.button("log me in scotty").clicked();
+            ui.label(format!("login error: {:?}", login_failure));
+        });
+    });
+    if login_now {
+        let result = handle_login(app_state.server, username, password);
+        if let LoginResult::Success = result {
+            app_state.menu = Menu::Contacts;
+        } else {
+            *login_failure = Some(result);
         }
-        main_menu_choice.clear();
     }
 }
-
-fn server_address_menu(server: &mut Option<SocketAddr>) {
-    match server {
-        Some(v) => println!("server: {}", v.ip()),
-        None => println!("server: unset"),
-    }
-    let mut buf = String::new();
-    loop {
-        print!("set server ip address: ");
-        std::io::stdout().flush().expect(lib::ERR_MSG_STDOUT);
-        std::io::stdin()
-            .read_line(&mut buf)
-            .expect(lib::ERR_MSG_STDIN);
-        if buf == "back\n" {
-            return;
-        }
-        match buf.trim().parse::<std::net::IpAddr>() {
-            Ok(v) => {
-                println!("{} server ip set to {v}", "success!".green());
-                *server = Some(SocketAddr::new(v, lib::PORT));
-                return;
-            }
-            Err(_) => {
-                println!("invalid ip. try again or go back with `back`");
-            }
-        }
-        buf.clear();
-    }
-}
-
-fn handle_login(socket: Option<SocketAddr>) {
-    let socket = match socket {
-        None => {
-            println!(
-                "{} no server defined; returning to previous menu",
-                "notice".yellow()
-            );
-            return;
-        }
+fn handle_login(
+    ip: Option<std::net::IpAddr>,
+    username: &mut String,
+    password: &mut String,
+) -> LoginResult {
+    let ip = match ip {
         Some(v) => v,
+        None => return LoginResult::NoServer
     };
-    let username = get_username();
-    let password = get_password();
-    let request = lib::Message::LoginRequest {
-        username,
-        password: lib::get_hash(password),
-    };
-    let mut stream = match std::net::TcpStream::connect(socket) {
+    let userhash = lib::get_hash(username);
+    let passhash = lib::get_hash(password);
+    username.clear();
+    password.clear();
+    let mut stream = match std::net::TcpStream::connect_timeout(&std::net::SocketAddr::new(ip, lib::PORT), std::time::Duration::from_secs(1)){
         Ok(v) => v,
-        Err(_) => return,
+        Err(_e) => return LoginResult::ConnectionTimeout
     };
-    lib::send_message(&mut stream, &request);
-    let message = match lib::get_message(&mut stream) {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("{} malformed request from server: {}", "error".red(), e);
-            return;
-        }
+    lib::send_message(
+        &mut stream,
+        &lib::Message::LoginRequest { username: userhash , password: passhash}
+    ).unwrap();
+    let response = match lib::get_message(&mut stream){
+        Ok(m) => m,
+        Err(_e) => return LoginResult::NetworkError,
     };
-
-    match message {
-        Message::LoginReply(reply) => match reply {
-            LoginResult::Accepted => {
-                println!("{}", "login accepted!".green())
-            }
-            LoginResult::BadPass => {
-                println!("{} bad password", "warning".red())
-            }
-            LoginResult::BadUser => {
-                println!("{} bad user", "warning".red())
+    match response{
+        Message::LoginReply(status) => {
+            match status{
+                lib::LoginStatus::Accepted => LoginResult::Success,
+                lib::LoginStatus::BadPass => LoginResult::BadPass,
+                lib::LoginStatus::BadUser => LoginResult::BadUser,
             }
         },
-        Message::BadRequest => {
-            println!(
-                "{}",
-                "we sent a request the server didn't understand!".red()
-            )
-        }
-        Message::LoginRequest {
-            username: _,
-            password: _,
-        } => {
-            println!("uh the server is asking us to login wtf")
-        }
-    }
+        _ => LoginResult::SomethingWentWrong,
 
-    lib::send_message(&mut stream, &request);
-}
-
-fn get_password() -> String {
-    let mut buf = String::new();
-    print!("password: ");
-    std::io::stdout().flush().expect(lib::ERR_MSG_STDOUT);
-    std::io::stdin()
-        .read_line(&mut buf)
-        .expect(lib::ERR_MSG_STDIN);
-    lib::get_hash(buf.trim().to_owned())
-}
-fn get_username() -> String {
-    loop {
-        let mut buf = String::new();
-        print!("username: ");
-        std::io::stdout().flush().expect(lib::ERR_MSG_STDOUT);
-        std::io::stdin()
-            .read_line(&mut buf)
-            .expect(lib::ERR_MSG_STDIN);
-        if buf.len() < lib::MAX_USERNAME_LEN {
-            return lib::get_hash(buf.trim().to_owned());
-        }
-        println!("20 character limit");
-        buf.clear();
     }
+}
+fn contacts_menu(app_state: &mut MyApp, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    egui::CentralPanel::default().show(ctx, |ui| {
+        ui.with_layout(egui::Layout::right_to_left(egui::Align::Min), |ui| {
+            if ui.button("log out").clicked() {
+                app_state.menu = Menu::Login {
+                    username: String::new(),
+                    password: String::new(),
+                    login_failure: None,
+                }
+            }
+        });
+        for i in 0..10 {
+            if ui.button(i.to_string()).clicked() {
+                app_state.menu = Menu::Chat { user_id: i }
+            };
+        }
+    });
+}
+fn chat_menu(app_state: &mut MyApp, ctx: &egui::Context, _frame: &mut eframe::Frame, chat_id: u32) {
+    egui::CentralPanel::default().show(ctx, |ui| {
+        if ui.button("back").clicked() {
+            app_state.menu = Menu::Contacts;
+        }
+        ui.vertical_centered(|ui| {
+            ui.label(format!("this is chat #{}!", chat_id));
+        })
+    });
 }
